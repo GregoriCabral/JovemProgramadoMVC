63c2038f21a6c76a4ef38de644b26aaf60c3267d
Gregori Cabral <gregorinovaes@outlook.com>
2024-08-13 21:31:23 -0300

    First commit

---
+++ JovenProgramadorMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JovenProgramadorMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
